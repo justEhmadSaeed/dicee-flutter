b072d2bf6c5fb7bf7bdfbc3edd7fa9ff29d17015
Ehmad <justEhmadSaeed@gmail.com>
2020-04-18 19:21:23 +0500

    👌 IMPROVE: Add Dice images

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,11 @@ class DicePage extends StatelessWidget {
       children: <Widget>[
         Expanded(
           flex: 2,
-          child: Image(image: AssetImage('images/dice1.png')),
+          child: Image.asset('images/dice1.png'),
         ),
         Expanded(
           flex: 1,
-          child: Image(image: AssetImage('images/dice2.png')),
+          child: Image.asset('images/dice2.png'),
         ),
       ],
     );
